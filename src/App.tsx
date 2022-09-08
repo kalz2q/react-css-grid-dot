df9e46eef6eaf4e9e31c03286972c9c109d9e81e
kalz2q <katsu95i@gmail.com>
2022-09-08 17:43:39 +0900

    m

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.title = "react-css-grid-dot"
+  })
   return (
     <div className="App">
+      <div className="box1">1</div>
+      <div className="box2">2</div>
+      <div className="box3">3</div>
+      <div className="box4">4</div>
+      <div className="box5">5</div>
     </div>
   );
 }
